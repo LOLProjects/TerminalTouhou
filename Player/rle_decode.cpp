c602490d3412378dce1c54376bfc9e9aee6bb6eb
Stellaris-code <Stellaris-code@users.noreply.github.com>
2020-03-16 22:00:23 +0100

    Create rle_decode.cpp

---
+++ Player/rle_decode.cpp
@@ -0,0 +1,92 @@
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+
+const uint8_t* read_input_file(const char* filename, int* size)
+{
+    *size = 0;
+    uint8_t *source = NULL;
+    FILE *fp = fopen(filename, "rb");
+    if (fp != NULL) {
+        /* Go to the end of the file. */
+        if (fseek(fp, 0L, SEEK_END) == 0) {
+            /* Get the size of the file. */
+            long bufsize = ftell(fp);
+            if (bufsize == -1)
+                return NULL;
+
+            /* Allocate our buffer to that size. */
+            source = (uint8_t*)malloc(sizeof(uint8_t) * bufsize);
+
+            /* Go back to the start of the file. */
+            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+
+            /* Read the entire file into memory. */
+            size_t newLen = fread(source, sizeof(uint8_t), bufsize, fp);
+            *size = newLen;
+            if ( ferror( fp ) != 0 ) {
+                fputs("Error reading file", stderr);
+                return NULL;
+            }
+        }
+        fclose(fp);
+    }
+
+    return source;
+}
+
+// return decode status
+int rle_decode_frame(const uint8_t** input, const uint8_t* end_of_input, uint8_t* output)
+{
+    int total_bytes = 0;
+
+    while (*input < end_of_input && total_bytes < 1920)
+    {
+        int count     = 1 + (*input)[0];
+        uint8_t byte  = (*input)[1];
+        memset(output, byte, count);
+        output += count;
+        total_bytes += count;
+
+        *input += 2;
+    }
+
+    if (total_bytes > 1920)
+    {
+        // frame overflow
+        return 1;
+    }
+    else if (total_bytes < 1920)
+    {
+        // early file end
+        return 2;
+    }
+
+    return 0;
+}
+
+// returns amount of decoded frames
+int rle_decode_file(const char* filename, uint8_t* frame_data)
+{
+    int size; const uint8_t* eof;
+    const uint8_t* data = read_input_file(filename, &size);
+    eof = data + size;
+
+    int decoded_frames = 0;
+    while (data < eof)
+    {
+        int status = rle_decode_frame(&data, eof, frame_data);
+        if (status != 0)
+        {
+            fprintf(stderr, "error at frame %d : '%s'\n", decoded_frames, (status == 1 ? "frame data overflow" : status == 2 ? "early frame data end" : "unknown"));
+            abort(); // abort
+            return 0;
+        }
+
+        frame_data += 1920;
+        ++decoded_frames;
+    }
+
+    return decoded_frames;
+}
